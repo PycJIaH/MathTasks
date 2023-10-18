971cc34c84dc62ce37e08efa00d67f6adbf90d4d
Ruslan Valeev <86679954+PycJIaH@users.noreply.github.com>
2023-10-18 00:08:15 -0600

    - Added Task 2

---
+++ MathTasks/Task_2_ForLoop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Math;
+using static MathTasks.Task_1_SimpleCalculationsFormulas;
+
+namespace MathTasks
+{
+    public static class Task_2_ForLoop
+    {
+        static void Main(string[] args)
+        {
+            for (double i = 0.55; i < 1.05; i += 0.05)
+            {
+                Console.WriteLine("Iteration with number {0}, result is {1}", i, MathEquation1(i));
+            }
+        }
+    }
+}
