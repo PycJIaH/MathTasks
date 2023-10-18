2ef4185f5e0661580ae011a891507cf2127a0c9d
Ruslan Valeev <86679954+PycJIaH@users.noreply.github.com>
2023-10-18 00:25:23 -0600

    Revert "- Fixed formulas and comments"
    This reverts commit 7ebfd367ceeb61a50dc4cf7b3c521dfee4d6b6cc.

--- MathTasks/Task_1_SimpleCalculationsFormulas.cs
+++ MathTasks/Task_1_SimpleCalculationsFormulas.cs
@@ -73,7 +73,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 4.
     public static double MathEquation4(double x)
     {
-        double result = (Sqrt(x) * Sin(Pow(x, 2) / 2.0) - 1.3) / (Pow(x, (1.0 / 5)) + Pow(E, 3 * x) + Abs(Cos(x)));
+        double result = (Sqrt(x) * Sin(Pow(x, 2) / 2) - 1.3) / (Pow(Sqrt(x), 5) + Pow(E, 3 * x) + Abs(Cos(x)));
 
         return result;
     }
@@ -81,7 +81,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 5.
     public static double MathEquation5(double x)
     {
-        double result = Sqrt(Pow(E, Abs(Sin(x)))) + (2 * Log(3 * x)) - (1.0 / 9);
+        double result = Sqrt(Pow(E, Abs(Pow(Sin(x), 3)))) + (2 * Log(3 * x)) - (1 / 9);
 
         return result;
     }
@@ -97,7 +97,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 7
     public static double MathEquation7(double x)
     {
-        double result = Sqrt((1.0 / 5) + Pow(Pow(E, x), (1.0 / 5))) / Abs(Log(Pow(x, 2)) - 1.3);
+        double result = Sqrt((1 / 5) + Pow(Sqrt(Pow(E, x)), 5)) / Abs(Log(Pow(x, 2)) - 1.3);
 
         return result;
     }
@@ -105,7 +105,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 8
     public static double MathEquation8(double x)
     {
-        double result = 1.8 + Log(Abs((4.0 + (2.0 / 7)) - Tan(Sin((5 * x) / 3.0))));
+        double result = 1.8 + Log(Abs((4 * (2 / 7)) - Tan(Sin((5 * x) / 3))));
 
         return result;
     }
@@ -113,7 +113,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 9
     public static double MathEquation9(double x)
     {
-        double result = (Abs(Sin(Sqrt(10.5 * x))) / (Pow(Pow(x, 2), (1.0 / 3)) - 0.143)) + 2 * PI * x;
+        double result = Abs(Sin(Sqrt(10.5 * x))) / (Pow(Sqrt(Pow(x, 2)), 3) - 0.143);
 
         return result;
     }
@@ -121,7 +121,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 10
     public static double MathEquation10(double x)
     {
-        double result = (1.1 * Pow(E, x)) + Abs(Cos(Sqrt(PI * x))) - (4.0 / 9);
+        double result = (1.1 * Pow(E, x)) + Abs(Cos(Sqrt(PI * x))) - (4 / 9);
 
         return result;
     }
@@ -129,7 +129,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 11
     public static double MathEquation11(double x)
     {
-        double result = (Log(Sqrt(Abs(x - 2)) + 1.2) / (2 + Pow(E, x))) + Pow((2.0 / x), (1.0 / 3));
+        double result = (Log(Sqrt(Abs(x - 2)) + 1.2) / (2 + Pow(E, x))) + Pow(Sqrt(2 / x), 3);
 
         return result;
     }
@@ -137,7 +137,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 12
     public static double MathEquation12(double x)
     {
-        double result = (1.0 / 3) * Sqrt(Abs(Sin(x))) * Pow(Pow(E, 0.12 * x), (1.0 / 3));
+        double result = Pow(Sqrt(Abs(Sin(x))), (1 / 3)) * Pow(Sqrt(Pow(E, 0.12 * x)), 3);
 
         return result;
     }
@@ -145,7 +145,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 13
     public static double MathEquation13(double x)
     {
-        double result = Pow(Pow(E, 2.0 / 3 - x), (1.0 / 5)) / Sqrt(Pow(x, 2) + Pow(x, 4) + Log(Abs(x - 3.4)));
+        double result = Pow(Sqrt(Pow(E, 2 / 3 - x)), 5) / Sqrt(Pow(x, 2) + Pow(x, 4) + Log(Abs(x - 3.4)));
 
         return result;
     }
@@ -153,7 +153,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 14
     public static double MathEquation14(double x)
     {
-        double result = (Sqrt(Pow(Sin(x / 2), 3)) + Pow(Pow(E, 1.3 * x) + Pow(E, -1.3 * x), (1.0 / 3))) / Abs(x - (7.0 / 9));
+        double result = (Sqrt(Pow(Sin(x / 2), 3)) + Pow(Sqrt(Pow(E, 1.3 * x) + Pow(E, -1 / 3 * x)), 3)) / Abs(x - (7 / 9));
 
         return result;
     }
@@ -161,7 +161,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 15
     public static double MathEquation15(double x)
     {
-        double result = (Abs(x * Log(x) - (4.0 / 7)) * Sqrt(x)) / Pow(Pow(E, 4 * x - 1.1), (1.0 / 5));
+        double result = (Abs(x * Log(x) - (4 / 7)) * Sqrt(x)) / Pow(Sqrt(Pow(E, 4 * x - 1.1)), 5);
 
         return result;
     }
@@ -169,7 +169,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 16
     public static double MathEquation16(double x)
     {
-        double result = Sqrt(Pow(E, 2 * x) * Sqrt(x) - ((x + (1.0 / 3)) / x)) * Abs(Cos(2.5 * x));
+        double result = Sqrt(Pow(E, 2 * x) * Sqrt(x) - ((x + (1 / 3)) / x)) * Abs(Cos(2.5 * x));
 
         return result;
     }
@@ -177,7 +177,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 17
     public static double MathEquation17(double x)
     {
-        double result = (Abs(7.2 - 10 * x) / Pow((x / 9) + Pow(E, 2 * x), (1.0 / 3))) * Atan((4 * Tan(2 * x)) / Sqrt(1.1 * Pow(x, 3)));
+        double result = (Abs(7.2 - 10 * x) / Pow(Sqrt((x / 9) + Pow(E, 2 * x)), 3)) * Atan((4 * Tan(2 * x)) / Sqrt(1.1 * Pow(x, 3)));
 
         return result;
     }
@@ -185,7 +185,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 18
     public static double MathEquation18(double x)
     {
-        double result = (Pow(x, 3) / Sqrt(3)) - Pow(E, x) * Log(Abs(Pow(1.37, 3) + Pow(x, 3))) + (4.0 / 3);
+        double result = (Pow(x, 3) / Sqrt(3)) - Pow(E, x) * Log(Abs(Pow(1.37, 3) + Pow(x, 3))) + (4 / 3);
 
         return result;
     }
@@ -193,7 +193,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 19
     public static double MathEquation19(double x)
     {
-        double result = (Log(Sqrt(PI + Abs(2 - x))) / (3 - 1.0 / x)) + Pow(Pow(x, 2), (1.0 / 3)) * Sin(1.4 * x);
+        double result = (Log(Sqrt(PI + Abs(2 - x))) / (3 - 1 / x)) + Pow(Sqrt(Pow(x, 2)), 3) * Sin(1.4 * x);
 
         return result;
     }
@@ -201,7 +201,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 20
     public static double MathEquation20(double x)
     {
-        double result = (Pow(Pow(Log(x), 2), (1.0 / 3)) + Tan(Cos(PI * x))) * Abs(Log(x / 10.5));
+        double result = (Pow(Sqrt(Pow(Log(x), 2)), 3) + Tan(Cos(PI * x))) * Abs(Log(x / 10.5));
 
         return result;
     }
@@ -209,7 +209,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 21
     public static double MathEquation21(double x)
     {
-        double result = (Sin(0.5 * PI * x) + Pow(x, (1.0 / 5))) / (Sqrt(Abs(Cos(PI * x)) + 1) * Pow(E, Sqrt(x)));
+        double result = (Sin(0.5 * PI * x) + Pow(Sqrt(x), 5)) / (Sqrt(Abs(Cos(PI * x)) + 1) * Pow(E, Sqrt(x)));
 
         return result;
     }
@@ -217,7 +217,8 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 22
     public static double MathEquation22(double x)
     {
-        double result = (1.0 / Sqrt(x)) + Pow(Log(Abs(0.2 + Sin(x))), 2) * Pow(Pow(x, 2), (1.0 / 3));
+        double result = (1 / Sqrt(x)) + Pow(Log(Abs(0.2 + Sin(x))), 2) * Pow(Sqrt(Pow(x, 2)), 3);
+        //double result = (1 / Sqrt(x)) + Pow(Log(Abs(0.2 + Sin(x)) * Pow(Sqrt(Pow(x, 2)), 3)), 2); - Возможен и такой вариант но вряд-ли
 
         return result;
     }
@@ -225,7 +226,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 23
     public static double MathEquation23(double x)
     {
-        double result = (Log(Sqrt(Pow(E, 0.1 * x) + x)) / (x + Pow(10.7, (1.0 / 3)) + Atan(x))) + (2.0 / 5);
+        double result = (Log(Sqrt(Pow(E, 0.1 * x))) / (x + Pow(Sqrt(10.7), 3) + Atan(x))) + (2 / 5);
 
         return result;
     }
@@ -233,7 +234,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 24
     public static double MathEquation24(double x)
     {
-        double result = (1.0 / 3) * Pow(Pow(E, 6.3 + Sqrt(x)), (1.0 / 7)) * Abs((Cos((2 * x) / 3.0) - x));
+        double result = (1 / 3) * Pow(Sqrt(Pow(E, 6.3 + Sqrt(x))), 7) * Abs((Cos(2 * x) / 3) - x);
 
         return result;
     }
@@ -241,7 +242,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 25
     public static double MathEquation25(double x)
     {
-        double result = (Abs(x - PI) * Pow(E, x / 3.0)) / Log(1.7 * Pow(x, (1.0 / 3)) + x * Sqrt(x));
+        double result = (Abs(x - PI) * Pow(E, x / 3)) / Log(1.7 * Pow(Sqrt(x), 3) + x * Sqrt(x));
 
         return result;
     }
@@ -257,7 +258,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 27
     public static double MathEquation27(double x)
     {
-        double result = (2 + (2.0 / 3) + Pow((x / 2.7), (1.0 / 3)) - Sqrt(Pow(E, -3 * x))) * Abs(Sin(5 * x));
+        double result = (2 * (2 / 3) + Pow(Sqrt(x / 2.7), 3) - Sqrt(Pow(E, -3 * x))) * Abs(Sin(5 * x));
 
         return result;
     }
@@ -273,7 +274,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 29
     public static double MathEquation29(double x)
     {
-        double result = (Abs(Log(Pow(x, 2))) + (1.0 / 3)) / Sqrt(Pow(E, x / PI) + Pow(x, (1.0 / 3)) + 1.4);
+        double result = (Abs(Log(Pow(x, 2))) + (1 / 3)) / Sqrt(Pow(E, x / PI) + Pow(Sqrt(x), 3) + 1.4);
 
         return result;
     }
@@ -281,7 +282,7 @@ public class Task_1_SimpleCalculationsFormulas
     //Exmaple number 30
     public static double MathEquation30(double x)
     {
-        double result = Pow(Pow(PI, 2) - Pow(x, 2) + (1.0 / E), (1.0 / 3)) + Tan((x - 1) / x) + (1.0 / 7);
+        double result = Pow(Sqrt(Pow(PI, 2) - Pow(x, 2) + (1 / E)), 3) + Tan((x - 1) / x) + (1 / 7);
 
         return result;
     }
