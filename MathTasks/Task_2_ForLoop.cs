501d796311faeb83b88b3a84ec508dc0e1093162
Ruslan Valeev <86679954+PycJIaH@users.noreply.github.com>
2024-01-10 02:52:36 -0700

    Solution for Task #2 (#5)
    * Solution for task #2
    * - Added unit test for issue 2
    * - minor code optimization

---
+++ MathTasks/Task_2_ForLoop.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using static System.Math;
+using static MathTasks.Task_1_SimpleCalculationsFormulas;
+
+namespace MathTasks
+{
+    public class Task_2_ForLoop
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<double, double> res = Task2Solution(ReadDoubleFromConsole(), ReadDoubleFromConsole(), ReadUIntFromConsole());
+
+            foreach (var item in res)
+            {
+                Console.WriteLine("For x = {0:N4}, f(x) = {1:N4}", item.Key, item.Value);
+            }
+        }
+
+        // Составить алгоритм и программу вычисления таблицы значений функции f(x)
+        // из задачи #1 для N значений аргумента X, равномерно распределенных на отрезке [A, B].
+        // Для проверки программы задать N = 10; A = 0,55; B = 1.
+        public static Dictionary<double, double> Task2Solution(double A, double B, uint N)
+        {
+            double increment = (B - A) / N;
+            Dictionary<double, double> result = new();
+
+            for (double x = A; x <= B; x += increment)
+            {
+                result[x] = MathEquation1(x);
+            }
+
+            return result;
+        }
+
+        public static uint ReadUIntFromConsole()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the integer value for equation: ");
+                    uint res = uint.Parse(Console.ReadLine()!.Replace(".", ","));
+                    Console.WriteLine("Input value: {0}", res);
+                    return res;
+                }
+                catch
+                {
+                    // throw new Exception("You need to enter a valid integer value! Repeat input!");
+                    Console.WriteLine("You need to enter a valid integer value! Repeat input!");
+                }
+            }
+        }
+    }
+}
---
+++ MathTasksTests/Task_2_ForLoopTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static MathTasks.Task_2_ForLoop;
+
+namespace MathTasks.Task_2_ForLoop_UnitTests
+{
+    [TestClass()]
+    public class Task_2_ForLoopTests
+    {
+        [TestMethod()]
+        public void Task2Solution_Test()
+        {
+            Dictionary<double, double> expectedDict = new()
+            {
+                { 0.55, 2.542574618653433 },
+                { 0.5950000000000001, 2.6281582048839494 },
+                { 0.6400000000000001, 2.722337633622857 },
+                { 0.6850000000000002, 2.824649451993068 },
+                { 0.7300000000000002, 2.9347633195293596 },
+                { 0.7750000000000002, 3.0524615354256195 },
+                { 0.8200000000000003, 3.177622241974861 },
+                { 0.8650000000000003, 3.310205731348277 },
+                { 0.9100000000000004, 3.4502433411247706 },
+                { 0.9550000000000004, 3.597828494118035 }
+            };
+            Dictionary<double, double> actualDict = Task2Solution(0.55, 1.0, 10);
+            expectedDict.Select(i => $"X = {i.Key}: Equality expected = {i.Value}").ToList().ForEach(Console.WriteLine);
+            Console.WriteLine("\n");
+            actualDict.Select(i => $"X = {i.Key}: Equality actual= {i.Value}").ToList().ForEach(Console.WriteLine);
+
+            Assert.IsTrue(expectedDict.SequenceEqual(actualDict));
+        }
+    }
+}
